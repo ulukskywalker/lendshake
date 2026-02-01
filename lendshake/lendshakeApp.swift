c3b15f9763538a36a92ad78d7028d2d2167f8ca1
ulukskywalker <ulukbek.abylbekov@icloud.com>
2026-02-01 17:00:04 -0600

    Initial Commit

---
+++ lendshake/lendshakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  lendshakeApp.swift
+//  lendshake
+//
+//  Created by Uluk Abylbekov on 2/1/26.
+//
+
+import SwiftUI
+
+@main
+struct lendshakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
